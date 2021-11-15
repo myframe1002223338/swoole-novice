f9d3fcdbafd3d9c4dceb9d8f9b818531bda7a075
17795695280 <1002223338@qq.com>
2021-11-15 17:52:39 +0800

    11/15

--- autoload.php
+++ autoload.php
@@ -4,11 +4,14 @@ date_default_timezone_set('Asia/chongqing');
 require_once('src'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config_db.php');
 @require_once('src'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config_swoole.php');
 use Swoole\Runtime;
-if(php_sapi_name()==='cli'){//只有在CLI模式运行下才执行IO异步非阻塞模式,否则会导致通过WEB服务器运行的客户端报错;
-    if(ASYNC_CO==1){//TCP等服务器中不支持协程容器,需要开启该方法自动实现IO异步非阻塞运行;
-        Runtime::enableCoroutine();
+if(extension_loaded('swoole')){
+    if(php_sapi_name()==='cli'){//只有在CLI模式运行下才执行IO异步非阻塞模式,否则会导致通过WEB服务器运行的客户端报错;
+        if(ASYNC_CO==1){//TCP等服务器中不支持协程容器,需要开启该方法自动实现IO异步非阻塞运行;
+            Runtime::enableCoroutine();
+        }
     }
 }
+
 function autoload(){
     require_once('src'.DIRECTORY_SEPARATOR.'Async.php');
     require_once('src'.DIRECTORY_SEPARATOR.'Async_mysql.php');
